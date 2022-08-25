723ae7703449a6d8ba72954638656ba4e422cf61
ThanhHauC <nguyenthanhhau.contact@gmail.com>
2022-08-25 21:05:44 +0700

    upload button flashsale

--- src/Images/index.js
+++ src/Images/index.js
@@ -1,11 +1,15 @@
 import IconTGDD from './iconnew.png';
 import ImageHome from './banner.png';
 import TopZone from './desk-1200x100-1200x100.png';
-import HeSale from './Trai-79x271-2.png';
+import TraiHeSale from './Trai-79x271-2.png';
+import PhaiHeSale from './Phai-79x271-7.png';
+import Lightning from './lightning-ic.png';
 
 export const Images = {
     IconTGDD,
     ImageHome,
     TopZone,
-    HeSale,
+    TraiHeSale,
+    PhaiHeSale,
+    Lightning,
 };
--- src/Pages/HomeFix/HomeFix.js
+++ src/Pages/HomeFix/HomeFix.js
@@ -26,9 +26,70 @@ const OPTION_PROMO = [
         title2: 'đồng hồ',
     },
 ];
+const BUTTON_TIMELINE = [
+    {
+        title: 'Đang diễn ra',
+        time: '19:00 - 21:00',
+    },
+    {
+        title: 'Ngày mai',
+        time: '09:00 - 11:00',
+    },
+    {
+        title: 'Đang diễn ra',
+        time: '14:00 - 16:00',
+    },
+];
 
 function HomeFix() {
     const [showimage, setShowImage] = useState('');
+    const [hour, setHour] = useState();
+    const [minute, setMinute] = useState();
+    const [second, setSecond] = useState();
+    const [timeline, setTimeLine] = useState(0);
+    let hourvalue = 1;
+    let minutevalue = 26;
+    let secondvalue = 43;
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 599) {
+                setShowImage('show-image-sale');
+            } else {
+                setShowImage('');
+            }
+        });
+    });
+    useEffect(() => {
+        setSecond(secondvalue);
+        setMinute(minutevalue);
+        setHour(hourvalue);
+        const timer = setInterval(() => {
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            secondvalue = secondvalue - 1;
+            secondvalue = `0${secondvalue}`.slice(-2);
+            setSecond(secondvalue);
+            if (secondvalue === '-1') {
+                secondvalue = 59;
+                setSecond(secondvalue);
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                minutevalue = minutevalue - 1;
+                minutevalue = `0${minutevalue}`.slice(-2);
+                setMinute(minutevalue);
+            }
+            if (minutevalue === '-1') {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                hourvalue = hourvalue - 1;
+                setHour(hourvalue);
+            }
+            if (hourvalue === '-1') {
+                hourvalue = 1;
+                setHour(hourvalue);
+            }
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        };
+    });
     const _handleOptionPromo = () => {
         return OPTION_PROMO.map((item, index) => (
             <li key={index}>
@@ -45,15 +106,18 @@ function HomeFix() {
             </li>
         ));
     };
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 599) {
-                setShowImage('show-image-sale');
-            } else {
-                setShowImage('');
-            }
-        });
-    });
+    const _handleButtonTimeLine = () => {
+        return BUTTON_TIMELINE.map((item, index) => (
+            <div
+                key={index}
+                className={cx(timeline === index && 'active-container')}
+                onClick={() => setTimeLine(index)}
+            >
+                <span className={cx('title-timeline', timeline === index && 'active')}>{item.title}</span>
+                <span className={cx('timeline', timeline === index && 'active')}>{item.time}</span>
+            </div>
+        ));
+    };
     return (
         <div className={cx('wrapper')}>
             <div className={cx('banner')}>
@@ -62,8 +126,8 @@ function HomeFix() {
             <div className={cx('main-container')}>
                 <div className={cx('image-sale-container', showimage)}>
                     <div className={cx('image-container')}>
-                        <img src={Images.HeSale} alt="" />
-                        <img src={Images.HeSale} alt="" />
+                        <img src={Images.TraiHeSale} alt="" />
+                        <img src={Images.PhaiHeSale} alt="" />
                     </div>
                 </div>
                 <div className={cx('slick-container')}>
@@ -72,6 +136,23 @@ function HomeFix() {
                 <ul className={cx('option-promo')}>{_handleOptionPromo()}</ul>
                 <div className={cx('topzane-container')}>
                     <img src={Images.TopZone} alt="" />
+                </div>
+                <div className={cx('flashsale-container')}>
+                    <div className={cx('gvdshock-container')}>
+                        <div className={cx('lightning-image')}>
+                            <img width="32" height="70" src={Images.Lightning} alt="lightning" />
+                        </div>
+                        <h3>GIỜ VÀNG DEAL SỐC</h3>
+                        <div className={cx('endtime-container')}>
+                            <span className={cx('label-endtime')}>Kết thúc trong</span>
+                            <span className={cx('coutdown-time')}>
+                                <label>0{hour}</label>
+                                <label>{minute}</label>
+                                <label>{second}</label>
+                            </span>
+                        </div>
+                    </div>
+                    <div className={cx('timeline-container')}>{_handleButtonTimeLine()}</div>
                 </div>
             </div>
         </div>
