3e67a1992b96e0f3019b0afdc47b224bd860aaa2
ThanhHauC <nguyenthanhhau.contact@gmail.com>
2022-08-25 16:10:36 +0700

    upload add image home

--- src/Images/index.js
+++ src/Images/index.js
@@ -1,7 +1,11 @@
 import IconTGDD from './iconnew.png';
 import ImageHome from './banner.png';
+import TopZone from './desk-1200x100-1200x100.png';
+import HeSale from './Trai-79x271-2.png';
 
 export const Images = {
     IconTGDD,
     ImageHome,
+    TopZone,
+    HeSale,
 };
--- src/Pages/HomeFix/HomeFix.js
+++ src/Pages/HomeFix/HomeFix.js
@@ -4,6 +4,7 @@ import { ImageOptionPormo } from '~/Images/OptionPromo';
 import { Images } from '~/Images';
 import Slick from '~/components/Slick';
 import { ImagesSlick } from '~/Images/Slick';
+import { useEffect, useState } from 'react';
 const cx = classNames.bind(styles);
 const OPTION_PROMO = [
     {
@@ -16,32 +17,62 @@ const OPTION_PROMO = [
     },
     {
         image: ImageOptionPormo.SaleHe,
-        title: 'Sale hè' + <br></br> + 'Giảm đến 50%',
+        title1: 'Sale hè',
+        title2: 'Giảm đến 50%',
     },
     {
         image: ImageOptionPormo.DongHo,
-        title: 'Bão sale' + <br></br> + 'đồng hồ',
+        title1: 'Bão sale',
+        title2: 'đồng hồ',
     },
 ];
 
 function HomeFix() {
+    const [showimage, setShowImage] = useState('');
     const _handleOptionPromo = () => {
         return OPTION_PROMO.map((item, index) => (
             <li key={index}>
                 <img src={item.image} alt="" />
+                {item.title ? (
+                    <span>{item.title}</span>
+                ) : (
+                    <span>
+                        {item.title1}
+                        <br></br>
+                        {item.title2}
+                    </span>
+                )}
             </li>
         ));
     };
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 599) {
+                setShowImage('show-image-sale');
+            } else {
+                setShowImage('');
+            }
+        });
+    });
     return (
         <div className={cx('wrapper')}>
             <div className={cx('banner')}>
                 <img width="1920" height="920" src={Images.ImageHome} alt="Banner Big Campaign" />
             </div>
             <div className={cx('main-container')}>
+                <div className={cx('image-sale-container', showimage)}>
+                    <div className={cx('image-container')}>
+                        <img src={Images.HeSale} alt="" />
+                        <img src={Images.HeSale} alt="" />
+                    </div>
+                </div>
                 <div className={cx('slick-container')}>
                     <Slick images={ImagesSlick} />
                 </div>
                 <ul className={cx('option-promo')}>{_handleOptionPromo()}</ul>
+                <div className={cx('topzane-container')}>
+                    <img src={Images.TopZone} alt="" />
+                </div>
             </div>
         </div>
     );
