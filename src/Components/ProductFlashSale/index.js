cc89ee6b2c031f531c53eb49f7dd0640327c7fe2
ThanhHauC <nguyenthanhhau.contact@gmail.com>
2022-08-28 14:46:59 +0700

    upload home goiyhomnay

---
+++ src/Components/ProductFlashSale/index.js
@@ -0,0 +1,7 @@
+import styles from './ProductFlashSale.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
+const ProductFlashSale = (images) => {};
+
+export default ProductFlashSale;
